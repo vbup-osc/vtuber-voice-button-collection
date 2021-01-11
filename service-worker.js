02ada37dab89c91e0982c0375273831328d97212
blacktunes <blacktunes@foxmail.com>
2021-01-12 00:08:07 +0800

    移除SW缓存

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,11 @@
-var cacheName = 'VVBC-V1.2'; //版本号
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName)
-        .then(cache => cache.addAll([
-            '/images/logo/logo016.png',
-            '/images/logo/logo072.png',
-            '/images/logo/logo152.png',
-            '/images/logo/logo192.png',
-            '/images/logo/logo256.png',
-            '/images/logo/logo512.png',
-        ]))
-    );
-});
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(r) {
-            console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r || fetch(e.request).then(function(response) {
-                return caches.open(cacheName).then(function(cache) {
-                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
-});
-self.addEventListener('install', function(event) {
-    event.waitUntil(self.skipWaiting());
-});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        Promise.all([
-            // 更新客户端
-            self.clients.claim(),
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+]);
 
-            // 清理旧版本
-            caches.keys().then(function(cacheList) {
-                return Promise.all(
-                    cacheList.map(function(cacheName) {
-                        console.log(cacheList);
-                        if (cacheName !== cacheList[0]) {
-                            console.log(cacheList);
-                            return caches.delete(cacheList[0]);
-                        }
-                    })
-                );
-            })
-        ])
-    );
-});
+workbox.core.setCacheNameDetails({ prefix: 'VTuberの音声ボタンコレクション' })
+
+workbox.core.skipWaiting()
+
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
