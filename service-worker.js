60e6b65ee28815a8290ff34aa8b4923ce07f4364
blacktunes <blacktunes@foxmail.com>
2021-01-12 14:36:10 +0800

    优化SW脚本

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,4 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
-self.__precacheManifest = (self.__precacheManifest || []).concat([
-]);
-
-workbox.core.setCacheNameDetails({ prefix: 'VTuberの音声ボタンコレクション' })
-
 workbox.core.skipWaiting()
-
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+workbox.precaching.precacheAndRoute([], {})
